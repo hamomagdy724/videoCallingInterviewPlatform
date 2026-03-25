999ecd010d2484c3728edb6e567230d1059829f7
hamomagdy724 <hamomagdy724@gmail.com>
2026-03-25 09:57:39 +0200

    Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,22 @@ import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
 import { Toaster } from "react-hot-toast";
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
 
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
     <>
       <Routes>
 
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={!isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
       </Routes>
 
@@ -21,6 +28,3 @@ function App() {
 }
 
 export default App;
-
-// tw, daisyui, react-router, react-hot-toast, 
-// todo: react-query aka tanstack query, axios
